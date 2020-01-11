d8b412808771f7a9838346b0f30b7ae41a802484
Ricardo Alonzo Ugalde <34896374+ricardo-alonzo-ugalde-EE@users.noreply.github.com>
2020-01-10 20:13:33 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _78_Introduction_to_Inheritance
+{   class Account // class named Account
+    {
+        private string id; // class data member string type named id
+        private decimal balance; // class data member deciaml type named balance
+
+        public Account() // default constructor named Account
+        {
+            id = " ";
+            balance = 0;
+        }
+        public Account(string idNum, decimal bal) // fully parametized constructor w/2 constructors
+        {
+            id = idNum;
+            balance = bal;
+        }
+        public void deposit(decimal amount)
+        {
+            balance += amount; // add amount and current balance and store in balance 
+        }
+        public void withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+        public override string ToString() // toString method 
+        {
+            return id + ": " + balance;
+        }
+
+    class CheckingAccount : Account // class inheritance from class Account to sub class CheckingAccount
+        {
+            public CheckingAccount(string idNum, decimal bal)
+                : base(idNum, bal)
+            {
+
+            }
+        }
+           
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acct = new Account("1", 100);
+            Console.WriteLine(acct.ToString());
+            acct.withdraw(10);
+            Console.WriteLine(acct.ToString());
+            acct.deposit(3000);
+            Console.WriteLine(acct.ToString());
+        }
+    }
+}
